49d0f4401a7058524bded97f256cf46a7113ac36
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-01-31 08:28:28 -0300

    usefull error middleware

---
+++ src/express/errors/base.ts
@@ -0,0 +1,20 @@
+export abstract class HttpBaseError extends Error {
+  public message: string;
+  public code: number;
+
+  constructor(message, code) {
+    super();
+
+    this.message = message;
+    this.code = code;
+  }
+
+  resultOutput() {
+    return {
+      error: {
+        code: this.code,
+        message: this.message,
+      },
+    };
+  }
+}
